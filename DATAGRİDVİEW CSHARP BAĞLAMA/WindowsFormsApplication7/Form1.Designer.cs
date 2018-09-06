7d94561fe4d2c6dfc46abfac8a2f239098f23cbd
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-06 15:00:04 +0300

    Add files via upload

---
+++ DATAGRİDVİEW CSHARP BAĞLAMA/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.vtDataSet = new WindowsFormsApplication7.vtDataSet();
+            this.kisiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kisiTableAdapter = new WindowsFormsApplication7.vtDataSetTableAdapters.kisiTableAdapter();
+            this.tableAdapterManager = new WindowsFormsApplication7.vtDataSetTableAdapters.TableAdapterManager();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.vtDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kisiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vtDataSet
+            // 
+            this.vtDataSet.DataSetName = "vtDataSet";
+            this.vtDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kisiBindingSource
+            // 
+            this.kisiBindingSource.DataMember = "kisi";
+            this.kisiBindingSource.DataSource = this.vtDataSet;
+            // 
+            // kisiTableAdapter
+            // 
+            this.kisiTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.kisiTableAdapter = this.kisiTableAdapter;
+            this.tableAdapterManager.UpdateOrder = WindowsFormsApplication7.vtDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(538, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(260, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.vtDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kisiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private vtDataSet vtDataSet;
+        private System.Windows.Forms.BindingSource kisiBindingSource;
+        private vtDataSetTableAdapters.kisiTableAdapter kisiTableAdapter;
+        private vtDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DATAGRİDVİEW CSHARP BAĞLAMA/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kisiBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*OleDbConnection bag = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=vt.accdb");
+            bag.Open();
+            OleDbDataAdapter kur = new OleDbDataAdapter("select * from kisi", bag);
+            DataSet hamal = new DataSet();
+            kur.Fill(hamal);
+            dataGridView1.DataSource = hamal.Tables[0];
+
+            */
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection bag = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=vt.accdb");
+            bag.Open();
+            OleDbDataAdapter kur = new OleDbDataAdapter("select * from kisi where ad='" +textBox1.Text+ "'",bag); 
+            DataSet hamal = new DataSet();
+            kur.Fill(hamal);
+            dataGridView1.DataSource = hamal.Tables[0];
+        }
+    }
+}
